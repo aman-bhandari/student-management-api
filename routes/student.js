77538a123545dece506e583923cf3b9ef8d8d152
Aman <bhandari.aman0101@gmail.com>
2022-09-29 14:49:57 +0530

    create student route

---
+++ routes/student.js
@@ -0,0 +1,6 @@
+const express = require(express)
+const router = express.Router()
+
+const { createStudent } = require('../controllers/student-controller')
+router.post('/', createStudent)
+module.exports = router
