8666a4c5c1c89e290f15365228cec9eea6efb794
Aman <bhandari.aman0101@gmail.com>
2022-09-29 15:30:34 +0530

    update student route and controller

--- controllers/student-controller.js
+++ controllers/student-controller.js
@@ -4,7 +4,17 @@ const createStudent = async (req, res) => {
   const student = await Student.create(req.body)
   res.status(200).json({ student })
 }
-const updateStudent = async (req, res) => {}
+const updateStudent = async (req, res) => {
+  const student = await Student.findOneAndUpdate(
+    {
+      email: req.body.email,
+    },
+    req.body,
+    { new: true, runValidators: true }
+  )
+  if (!student) throw new Error('no student')
+  res.status(200).json({ student })
+}
 const deleteStudent = async (req, res) => {
   const student = await Student.findOneAndDelete({ mobile: req.body.mobile })
   if (!student) throw new Error('no student')
--- routes/student.js
+++ routes/student.js
@@ -4,7 +4,9 @@ const router = express.Router()
 const {
   createStudent,
   deleteStudent,
+  updateStudent,
 } = require('../controllers/student-controller')
 router.post('/', createStudent)
 router.delete('/', deleteStudent)
+router.patch('/', updateStudent)
 module.exports = router
